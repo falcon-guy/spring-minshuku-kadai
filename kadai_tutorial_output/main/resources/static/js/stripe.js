461a9352dd03b9b89027095a1cc13204fab2e097
falcon-guy <farukon-gai@ezweb.ne.jp>
2025-03-29 15:42:20 +0900

    Delete kadai_tutorial_output/main directory

--- kadai_tutorial_output/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51R0GESCAUyozOywN6F9BTxqdafvjrHZbiMPCggjOT4FDVGJB3HFc7yN3xhATtwfeyi7e2fV4GA9sdeeZFtx9GvUr00Uyu4YBVC');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
